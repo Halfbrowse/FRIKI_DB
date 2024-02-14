cbabb9e2d4423270b67e9438ded830ed3187f564
Eoin Byrne <eoinbye@gmail.com>
2024-02-14 19:31:39 +0100

    adddded slqalchemy

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
-import psycopg2
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Text, inspect
+from sqlalchemy.exc import NoSuchTableError
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 import os
 import pandas as pd
-import psycopg2
 from contextlib import contextmanager
 import streamlit as st
+
 
 load_dotenv()
 external_db_url = os.getenv("EXTERNAL_DB_URL")
@@ -15,95 +17,101 @@ db_name = os.getenv("DB_NAME")
 
 
 def create_database():
-    external_db_url = os.getenv("EXTERNAL_DB_URL")
-    conn = psycopg2.connect(
-        f"postgres://{db_user}:{db_password}@{external_db_url}/friki_db"
-    )
-    c = conn.cursor()
+    engine = create_engine(external_db_url)
+    metadata = MetaData()
 
-    c.execute(
-        """
-        CREATE TABLE IF NOT EXISTS Entities (
-            id SERIAL PRIMARY KEY,
-            Name TEXT,
-            Alias TEXT,
-            Type TEXT,
-            Attribution TEXT,
-            Attribution_links TEXT,
-            Attribution_type TEXT,
-            Attribution_confidence INTEGER,
-            Label TEXT,
-            Parent_actor TEXT,
-            Subsidiary_actors TEXT,
-            Threat_Actor TEXT,
-            TTPs TEXT,
-            Description_of_TTPs TEXT,
-            Description_TTPs_Link TEXT,
-            Master_Narratives TEXT,
-            Master_Narrative_Description TEXT,
-            Master_Narrative_Links TEXT,
-            Summary TEXT,
-            External_links TEXT,
-            Language TEXT,
-            Country TEXT,
-            Sub_region TEXT,
-            Region TEXT,
-            Website TEXT,
-            Twitter TEXT,
-            Twitter_ID TEXT,
-            Facebook TEXT,
-            Threads TEXT,
-            YouTube TEXT,
-            YouTube_ID TEXT,
-            TikTok TEXT,
-            Instagram TEXT,
-            LinkedIn TEXT,
-            Reddit TEXT,
-            VK TEXT,
-            Telegram TEXT,
-            Substack TEXT,
-            Quora TEXT,
-            Patreon TEXT,
-            GoFundMe TEXT,
-            Paypal TEXT,
-            Twitch TEXT,
-            Mastadon TEXT,
-            Wechat TEXT,
-            QQ TEXT,
-            Douyin TEXT
-        )
-    """
+    # Define the table structure
+    entities_table = Table(
+        "Entities",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("Name", Text),
+        Column("Alias", Text),
+        Column("Type", Text),
+        Column("Attribution", Text),
+        Column("Attribution_links", Text),
+        Column("Attribution_type", Text),
+        Column("Attribution_confidence", Integer),
+        Column("Label", Text),
+        Column("Parent_actor", Text),
+        Column("Subsidiary_actors", Text),
+        Column("Threat_Actor", Text),
+        Column("TTPs", Text),
+        Column("Description_of_TTPs", Text),
+        Column("Description_TTPs_Link", Text),
+        Column("Master_Narratives", Text),
+        Column("Master_Narrative_Description", Text),
+        Column("Master_Narrative_Links", Text),
+        Column("Summary", Text),
+        Column("External_links", Text),
+        Column("Language", Text),
+        Column("Country", Text),
+        Column("Sub_region", Text),
+        Column("Region", Text),
+        Column("Website", Text),
+        Column("Twitter", Text),
+        Column("Twitter_ID", Text),
+        Column("Facebook", Text),
+        Column("Threads", Text),
+        Column("YouTube", Text),
+        Column("YouTube_ID", Text),
+        Column("TikTok", Text),
+        Column("Instagram", Text),
+        Column("LinkedIn", Text),
+        Column("Reddit", Text),
+        Column("VK", Text),
+        Column("Telegram", Text),
+        Column("Substack", Text),
+        Column("Quora", Text),
+        Column("Patreon", Text),
+        Column("GoFundMe", Text),
+        Column("Paypal", Text),
+        Column("Twitch", Text),
+        Column("Mastadon", Text),
+        Column("Wechat", Text),
+        Column("QQ", Text),
+        Column("Douyin", Text),
     )
 
-    conn.commit()
-    conn.close()
-    print("Database created!")
+    # Check if table exists and has the required columns
+    inspector = inspect(engine)
+    try:
+        existing_columns = [col["name"] for col in inspector.get_columns("Entities")]
+        required_columns = [column.name for column in entities_table.columns]
+        if not all(col in existing_columns for col in required_columns):
+            print("Required columns not found. Creating/Updating table...")
+            metadata.create_all(engine)
+        else:
+            print("All required columns are present.")
+    except NoSuchTableError:
+        print("Table not found. Creating new table...")
+        metadata.create_all(engine)
 
 
 def connect_to_database():
-    external_db_url = os.getenv("EXTERNAL_DB_URL")
-    db_user = os.getenv("DB_USER")
-    db_password = os.getenv("DB_PASSWORD")
-    conn = psycopg2.connect(
-        f"postgres://{db_user}:{db_password}@{external_db_url}/friki_db"
-    )
-    return conn
+    return create_engine(external_db_url)
 
 
+# Context manager for SQLAlchemy engine
 @contextmanager
-def connect_db():
-    conn = psycopg2.connect(
-        f"postgres://{db_user}:{db_password}@{external_db_url}/{db_name}"
-    )
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    engine = connect_to_database()
+    connection = engine.connect()
     try:
-        yield conn
+        yield connection
+        connection.commit()
+    except:
+        connection.rollback()
+        raise
     finally:
-        conn.close()
+        connection.close()
 
 
+# Refactored functions
 def get_entity(id):
     try:
-        with connect_db() as conn:
+        with session_scope() as conn:
             return pd.read_sql_query(f"SELECT * FROM Entities WHERE id = {id}", conn)
     except Exception as e:
         print("Error in get_entity:", e)
@@ -112,13 +120,9 @@ def get_entity(id):
 
 def add_entity(data):
     try:
-        with connect_db() as conn:
+        with session_scope() as conn:
             placeholders = ", ".join(["%s"] * len(data))
-            cursor = conn.cursor()
-            cursor.execute(
-                f"INSERT INTO Entities VALUES (DEFAULT, {placeholders})", data
-            )
-            conn.commit()
+            conn.execute(f"INSERT INTO Entities VALUES (DEFAULT, {placeholders})", data)
     except Exception as e:
         print("Error in add_entity:", e)
 
@@ -128,29 +132,25 @@ def delete_entity(id):
         if not entity_exists(id):
             return st.error(f"Entity with id {id} does not exist.")
 
-        with connect_db() as conn:
+        with session_scope() as conn:
             conn.execute(f"DELETE FROM Entities WHERE id = {id}")
-            conn.commit()
-            print("Deleted entity with id:", id)
     except Exception as e:
         print("Error in delete_entity:", e)
 
 
 def entity_exists(id):
-    with connect_db() as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT 1 FROM Entities WHERE id = %s", (id,))
-        return cursor.fetchone() is not None
+    with session_scope() as conn:
+        result = conn.execute("SELECT 1 FROM Entities WHERE id = %s", (id,))
+        return result.fetchone() is not None
 
 
 def get_entities_df():
     try:
-        with connect_db() as conn:
-            cursor = conn.cursor()
-            cursor.execute(
+        with session_scope() as conn:
+            result = conn.execute(
                 "SELECT table_name FROM information_schema.tables WHERE table_name = 'Entities'"
             )
-            if cursor.fetchone():
+            if result.fetchone():
                 return pd.read_sql_query("SELECT * FROM Entities", conn)
             else:
                 print(
@@ -164,19 +164,11 @@ def get_entities_df():
 
 def bulk_insert_entities(df):
     try:
-        with connect_db() as conn:
+        with session_scope() as conn:
             df.columns = [col.replace(" ", "_") for col in df.columns]
             df.to_sql("Entities", conn, if_exists="append", index=False)
-            conn.commit()
     except Exception as e:
         print("Error in bulk_insert_entities:", e)
-
-
-def get_entities_df_with_selection():
-    df = get_entities_df()
-    if not df.empty:
-        df.insert(0, "Select", [False] * len(df))
-    return df
 
 
 def update_entity(id, data):
@@ -184,16 +176,11 @@ def update_entity(id, data):
         if not entity_exists(id):
             return st.error(f"Entity with id {id} does not exist.")
 
-        with connect_db() as conn:
-            placeholders = ", ".join(f"{col} = ?" for col in data)
+        with session_scope() as conn:
+            placeholders = ", ".join(f"{col} = %s" for col in data)
             conn.execute(
                 f"UPDATE Entities SET {placeholders} WHERE id = {id}",
                 list(data.values()),
             )
-            conn.commit()
     except Exception as e:
         print("Error in update_entity:", e)
-
-
-if __name__ == "__main__":
-    create_database()
